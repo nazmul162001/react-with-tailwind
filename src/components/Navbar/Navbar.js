057e7208a856d9e75c1f8eae1c688538c8a53d12
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-27 23:27:03 +0600

    added humberger menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Lilnk from '../Link/Lilnk';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   const routes = [
     {id: 1, name: 'Home', link: '/home'},
     {id: 2, name: 'Shop', link: '/shop'},
@@ -10,8 +12,11 @@ const Navbar = () => {
     {id: 5, name: 'Contact', link: '/contact'}
   ]
   return (
-    <nav>
-      <ul className='md:flex justify-center py-10'>
+    <nav className='bg-indigo-300'>
+      <div onClick={() => setOpen(!open)} className='w-6 h-6 md:hidden'>
+        {open ? <XIcon /> : <MenuIcon />}
+      </div>
+      <ul className={`md:flex md:static justify-center absolute duration-500 ease-in bg-indigo-300 w-full ${open ? 'top-6' : 'top-[-120px]' }`}>
           {
             routes.map(route => <Lilnk 
             key = {route.id}
